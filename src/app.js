c91fa61093d1682526da951f510d7e14c098b179
Roberto Achar <robertoachar@gmail.com>
2019-05-03 22:53:08 -0300

    feature: add graphql

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ import cors from '@koa/cors';
 import helmet from 'koa-helmet';
 
 import router from './router';
+import graphqlRouter from './graphql';
 
 const app = new Koa();
+
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser());
 app.use(router.routes(), router.allowedMethods());
+app.use(graphqlRouter.routes(), graphqlRouter.allowedMethods());
 
 export default app;
---
+++ src/graphql.js
@@ -0,0 +1,36 @@
+import KoaRouter from 'koa-router';
+import graphqlHttp from 'koa-graphql';
+import { buildSchema } from 'graphql';
+
+const router = KoaRouter();
+
+router.post(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+      type Queries {
+        hello: String!
+      }
+
+      type Mutations {
+        sayHello(name: String!): String!
+      }
+
+      schema {
+        query: Queries
+        mutation: Mutations
+      }
+    `),
+    rootValue: {
+      hello: () => {
+        return 'Hello GraphQL';
+      },
+      sayHello({ name }) {
+        return `Hello ${name}`;
+      }
+    },
+    graphiql: true
+  })
+);
+
+export default router;
